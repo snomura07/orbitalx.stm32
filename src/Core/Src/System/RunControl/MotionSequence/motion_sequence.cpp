a82f3f87d5763f0742ae9eba8b884da945355049
snomura <you@example.com>
2026-05-24 19:57:05 +0900

    update

--- src/Core/Src/System/RunControl/MotionSequence/motion_sequence.cpp
+++ src/Core/Src/System/RunControl/MotionSequence/motion_sequence.cpp
@@ -2,14 +2,68 @@
 
 namespace RunControl
 {
-MotionSequence::MotionSequence(){}
+MotionSequence::MotionSequence():
+    currMotion(MotionKind::STOP),
+    nextMotion(MotionKind::STOP){
+
+}
+
 MotionSequence::~MotionSequence(){}
 
 void MotionSequence::init(){
 }
 
 void MotionSequence::update(){
+    // start condition: STRAIGHT
+    // end condition
 
+    if(endCheck()){
+        currMotion = nextMotion;
+
+    }
 }
+
+bool MotionSequence::endCheck(){
+    switch (currMotion)
+    {
+        case MotionKind::STRAIGHT:
+        // end condition: 直進距離が一定以上
+        break;
+
+        case MotionKind::TURN_LEFT:
+        // end condition: 左旋回角度が一定以上
+        break;
+
+        case MotionKind::TURN_RIGHT:
+        // end condition: 右旋回角度が一定以上
+        break;
+
+        case MotionKind::STOP:
+        // end condition: 停止時間が一定以上
+        break;
+
+        default:
+        break;
+    }
+
+    return true;
+}
+
+
+void MotionSequence::pushNextMotion(MotionKind motion){
+    nextMotion = motion;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/Core/Src/System/RunControl/MotionSequence/motion_sequence.h
+++ src/Core/Src/System/RunControl/MotionSequence/motion_sequence.h
@@ -5,11 +5,28 @@ namespace RunControl
 {
 class MotionSequence {
 public:
+    enum MotionKind
+    {
+        STRAIGHT,
+        TURN_LEFT,
+        TURN_RIGHT,
+        STOP
+    };
+
+public:
     MotionSequence();
     ~MotionSequence();
-
     void init();
     void update();
+    void pushNextMotion(MotionKind motion);
+
+private:
+    void start();
+    bool endCheck();
+
+private:
+    MotionKind currMotion;
+    MotionKind nextMotion;
 
 };
 }
